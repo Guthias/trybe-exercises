4384ae787469630a50f1c7058392c1bc973f8c32
guthias <gmgustavom@gmail.com>
2021-11-05 16:33:48 -0300

    Adicionando exercicios b4d1e2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-02/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-02/scripts.js
@@ -5,3 +5,27 @@ const patientInfo = {
   lastName: 'Santos',
 };
 const patientEmail = 'ana@email.com';
+
+console.log("----- Verificando tipo das Variaveis -----");
+console.log("patiendId -> ", typeof patientId);
+console.log("isEnrolled -> ", typeof isEnrolled);
+console.log("patiendInfo -> ", typeof patientInfo);
+console.log("patiendEmail -> ", typeof patientEmail);
+
+console.log("\n----- Checando patientAge -----");
+console.log("patientAge -> ", typeof patientAge);
+
+console.log("\n----- Alterando o patientId -----");
+patientId = '50';
+console.log("patientId -> ", typeof patientId);
+
+console.log("\n----- Calculando area do Retângulo -----");
+const base = 5;
+let heigth = 8;
+const area = base * heigth;
+const perimeter = base * 2 + heigth * 2;
+
+console.log("Base -> ", base);
+console.log("Altura -> ", heigth);
+console.log("Área -> ", area);
+console.log("Perimetro -> ", perimeter);
