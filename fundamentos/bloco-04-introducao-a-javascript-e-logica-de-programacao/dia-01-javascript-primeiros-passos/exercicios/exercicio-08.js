4ca2c9219440fb706f6d8c85e2b245b09688d68c
guthias <gmgustavom@gmail.com>
2021-11-07 17:07:30 -0300

    Corrigindo b4d1e8

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-08.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-08.js
@@ -2,7 +2,7 @@ const a = 10;
 const b = 21;
 const c = 11;
 
-if (a % 2 == 0 || b % 2 == 0 && c % 2 == 0 ) {
+if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0 ) {
   console.log(true);
 } else {
   console.log(false);
