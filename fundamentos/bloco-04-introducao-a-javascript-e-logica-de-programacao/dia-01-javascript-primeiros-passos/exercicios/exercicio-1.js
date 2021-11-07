281b7be46265aaa19ecd7a3104a4f6efe4bd41fe
guthias <gmgustavom@gmail.com>
2021-11-07 02:02:18 -0300

    Realizando o b4d1e1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios/exercicio-1.js
@@ -0,0 +1,21 @@
+const a = 20;
+const b = 15;
+
+console.log("----- Adição -----");
+console.log(`${a} + ${b} = ${a+b}`);
+
+
+console.log("\n----- Subtração -----");
+console.log(`${a} + ${b} = ${a-b}`);
+
+
+console.log("\n----- Multiplacação -----");
+console.log(`${a} * ${b} = ${a*b}`);
+
+
+console.log("\n----- Divisão -----");
+console.log(`${a} / ${b} = ${a/b}`);
+
+
+console.log("\n----- Módulo -----");
+console.log(`${a} % ${b} = ${a%b}`);
