02cb2dba6103c767a049864c455086b9cb5f9cb7
guthias <gmgustavom@gmail.com>
2021-11-09 12:27:01 -0300

    Realizando o b4d2e5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/exercicios/exercicio-5.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let maiorNumero;
+
+for (number of numbers){
+  if (number > maiorNumero || maiorNumero == undefined) {
+    maiorNumero = number;
+  }
+}
+
+console.log(`O maior numero é ${maiorNumero}`);
