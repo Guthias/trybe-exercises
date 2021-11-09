b55e1b3489b2f0cac36a16b9e271f8f7552a02f8
guthias <gmgustavom@gmail.com>
2021-11-09 12:04:13 -0300

    Relizando b4d2e2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/exercicios/exercicio-2.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaTotal = 0;
+
+for (number of numbers){
+  somaTotal += number;
+}
+
+console.log(`A soma total dos numeros é ${somaTotal}`);
