742b8bec1d9d64262f287455a0cfc621c9666953
guthias <gmgustavom@gmail.com>
2021-11-09 18:40:56 -0300

    Refatorando o for b4d3e1

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-e-algoritmos/exercicios/exercicio-1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-e-algoritmos/exercicios/exercicio-1.js
@@ -2,9 +2,9 @@ const n = 7;
 let line = "";
 
 if (n > 1){
-  for (let i = 0; i < n; i+=1){
+  for (let y = 0; y < n; y+=1){
     line = "";
-    for (let i = 0; i < n; i += 1){
+    for (let x = 0; x < n; x += 1){
       line += "*";
     }
     console.log(line);
