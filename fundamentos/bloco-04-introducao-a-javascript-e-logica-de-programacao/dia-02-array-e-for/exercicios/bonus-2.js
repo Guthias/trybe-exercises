c150d779434a15315a5e60230c8b12eabbf96fa2
guthias <gmgustavom@gmail.com>
2021-11-09 16:29:40 -0300

    Realizando b4d2 bonus 2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-for/exercicios/bonus-2.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let count = 1; count < numbers.length; count+=1){
+  for (let index = 0; index < count; index+=1){
+    if (numbers[count] > numbers[index]){
+      let numero = numbers[count];
+      numbers[count] = numbers[index];
+      numbers[index] = numero;
+    }
+  }
+}
+
+console.log(numbers)
