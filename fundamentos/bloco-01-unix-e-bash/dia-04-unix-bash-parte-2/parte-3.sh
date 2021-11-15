4fb940edb6df46216ad3888a7846a526e82753d6
guthias <gmgustavom@gmail.com>
2021-11-15 20:57:27 -0300

    Removendo ultimos exercicios da parte-3  b1d3

--- fundamentos/bloco-01-unix-e-bash/dia-04-unix-bash-parte-2/parte-3.sh
+++ fundamentos/bloco-01-unix-e-bash/dia-04-unix-bash-parte-2/parte-3.sh
@@ -26,33 +26,4 @@ kill $PIDsleep
 
 clear
 
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-# PENSAR EM NOVA FORMA PARA CONTINUAR SEM INTERFERIR A EXECUÇÃO DO ARQUIVO
-
-#Exercicio 4
-echo "4) Execute novamente o comando sleep 30, mas agora sem o &. Depois, faça com que ele continue executando em background\n"
-
-#Exercicio 5
-echo "5) Crie um processo em background que rode o comando sleep por 300 segundos\n"
-
-#Exercicio 6
-echo "6) Crie mais dois processos que rodem o comando sleep por 200 e 100 segundos, respectivamente"
-
-#Exercicio 7
-echo "7) Verifique que apenas o processo sleep 300 está em execução com o comando jobs. Suspenda a execução desse processo\n"
-
-#Exercicio 8
-echo "8) Retome a execução do processo sleep 100 em background com o comando bg"
-
-#Ecercicio 9
-echo "9) Termine a execução de todos os processos sleep (mate os processos"
+# NÃO É POSSIVEL SOLUCIONAR OS PROXIMOS EXERCICIOS VIA .sh
