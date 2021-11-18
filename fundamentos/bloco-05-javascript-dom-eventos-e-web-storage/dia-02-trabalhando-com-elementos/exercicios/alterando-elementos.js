3d1ea39ee4557c279453ce14dde8ca4ce36c6ad3
guthias <gmgustavom@gmail.com>
2021-11-17 23:43:50 -0300

    Realizando b5d2e11

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/exercicios/alterando-elementos.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/exercicios/alterando-elementos.js
@@ -1,3 +1,9 @@
 // Colocando classe no h1
 let element = document.getElementsByTagName('h1')[0];
 element.className = 'title';
+
+// Adicionando classe h3 nas 3 h3 criadas
+let elements = document.getElementsByTagName('h3');
+for (let item of elements){
+  item.className = 'description'
+}
