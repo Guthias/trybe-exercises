29cb2fc57ac1dae38a89c0b2efc23dce3e4b131b
guthias <gmgustavom@gmail.com>
2021-11-18 20:00:03 -0300

    Realizando b5d3e1

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-eventos/exercicios/script.js
@@ -16,3 +16,31 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo
+
+// 1) Criando dias do mês
+function createDaysofTheMonth(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const holiday = [24, 25, 31];
+  const friday = [4, 11, 18, 25];
+
+  let days = document.getElementById('days');
+  let element;
+
+  for (day of dezDaysList){
+    element = document.createElement('li');
+    element.className = 'day';
+    element.innerText = day;
+
+    if (holiday.includes(day)){
+      element.classList.add('holiday');
+    }
+
+    if (friday.includes(day)){
+      element.classList.add('friday');
+    }
+
+    days.appendChild(element);
+  }
+}
+
+createDaysofTheMonth();
