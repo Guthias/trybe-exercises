619c9b873e7c429c2a9b95f770b4f5bc2c81ad84
guthias <gmgustavom@gmail.com>
2021-11-20 00:00:57 -0300

    Adicionando alteração na fonte b5d4

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
@@ -3,7 +3,9 @@ const mainContent = document.getElementById('main-content');
 const backgroundSelect = document.getElementById('background-select');
 const colorSelect = document.getElementById('color-select');
 const sizeSelect = document.getElementById('size-select');
-const lineHeightSelect = document.getElementById('line-height-select')
+const lineHeightSelect = document.getElementById('line-height-select');
+const fontFamilySelect = document.getElementById('font-family-select');
+
 // Criando classes para alterar os elementos
 function changeBackgroundColor(event) {
   mainContent.style.backgroundColor = event.target.value;
@@ -23,8 +25,14 @@ function changeLineHeight (event) {
   mainContent.style.lineHeight = lineHeight + 'em';
 }
 
+function changeFontFamily (event) {
+  let fontFamily = event.target.value;
+  mainContent.style.fontFamily = fontFamily;
+}
+
 // Adicionando Eventos aos elementos
 backgroundSelect.addEventListener('change', changeBackgroundColor);
 colorSelect.addEventListener('change', changeFontColor);
 sizeSelect.addEventListener('change', changeFontSize);
 lineHeightSelect.addEventListener('change', changeLineHeight)
+fontFamilySelect.addEventListener('change', changeFontFamily)
