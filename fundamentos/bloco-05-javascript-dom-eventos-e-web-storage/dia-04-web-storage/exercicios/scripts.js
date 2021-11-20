ebd072c3ab46d6afa3e7f50bb2e810f1528d200a
guthias <gmgustavom@gmail.com>
2021-11-19 23:28:43 -0300

    Alterando a cor da fonte b5d4

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
@@ -1,9 +1,17 @@
-let mainContent = document.getElementById('main-content')
+// Transformando os IDS do HTML em variaveis
+const mainContent = document.getElementById('main-content');
+const backgroundSelect = document.getElementById('background-select');
+const colorSelect = document.getElementById('color-select');
 
+// Criando classes para alterar os elementos
 function changeBackgroundColor(event) {
   mainContent.style.backgroundColor = event.target.value;
 }
 
-let backgroundSelect = document.getElementById('background-select')
+function changeFontColor(event) {
+  mainContent.style.color = event.target.value;
+}
 
+// Adicionando Eventos aos elementos
 backgroundSelect.addEventListener('change', changeBackgroundColor);
+colorSelect.addEventListener('change', changeFontColor);
