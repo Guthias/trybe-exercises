6509d3ec58eba93667031e2c2bec0762c6b906b8
guthias <gmgustavom@gmail.com>
2021-11-19 23:53:12 -0300

    Adicionando mudança ao espaçamento de linhas

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/scripts.js
@@ -3,7 +3,7 @@ const mainContent = document.getElementById('main-content');
 const backgroundSelect = document.getElementById('background-select');
 const colorSelect = document.getElementById('color-select');
 const sizeSelect = document.getElementById('size-select');
-
+const lineHeightSelect = document.getElementById('line-height-select')
 // Criando classes para alterar os elementos
 function changeBackgroundColor(event) {
   mainContent.style.backgroundColor = event.target.value;
@@ -18,7 +18,13 @@ function changeFontSize (event) {
   mainContent.style.fontSize = fontSize + 'px';
 }
 
+function changeLineHeight (event) {
+  let lineHeight = event.target.value;
+  mainContent.style.lineHeight = lineHeight + 'em';
+}
+
 // Adicionando Eventos aos elementos
 backgroundSelect.addEventListener('change', changeBackgroundColor);
 colorSelect.addEventListener('change', changeFontColor);
-sizeSelect.addEventListener('change', changeFontSize)
+sizeSelect.addEventListener('change', changeFontSize);
+lineHeightSelect.addEventListener('change', changeLineHeight)
