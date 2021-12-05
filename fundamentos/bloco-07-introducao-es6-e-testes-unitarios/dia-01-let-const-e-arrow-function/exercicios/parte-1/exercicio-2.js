befc00aea7752458ccd97b0bddf339c9d6da46c7
guthias <gmgustavom@gmail.com>
2021-12-05 20:25:07 -0300

    Realizando b7d1p1e2

--- fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-01-let-const-e-arrow-function/exercicios/parte-1/exercicio-2.js
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-01-let-const-e-arrow-function/exercicios/parte-1/exercicio-2.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
-
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+const sortArray = array => array.sort((a, b) => a - b);
+ 
+console.log(sortArray(oddsAndEvens));
