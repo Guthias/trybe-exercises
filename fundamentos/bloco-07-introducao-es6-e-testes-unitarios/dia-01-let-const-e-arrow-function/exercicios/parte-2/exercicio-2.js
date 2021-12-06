12087a0c140c2abe1adc0a18f822ae0388f2890e
guthias <gmgustavom@gmail.com>
2021-12-06 19:08:14 -0300

    Simplificando a function b7d1p2e2

--- fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-01-let-const-e-arrow-function/exercicios/parte-2/exercicio-2.js
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-01-let-const-e-arrow-function/exercicios/parte-2/exercicio-2.js
@@ -1,8 +1,4 @@
-const longestWord = (phrase) => {
-  let words = phrase.split(' ');
-  words = words.sort((a, b) => b.length - a.length);
-  return words[0]
-}
+const longestWord = phrase => phrase.split(' ').sort((a, b) => b.length - a.length)[0];
 
 console.log(longestWord('O importante não é vencer todos os dias, mas lutar sempre.')) // importante
 console.log(longestWord('O medo de perder tira a vontade de ganhar.')) // vontade
