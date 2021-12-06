76fa8ca6bc5e6121a70763b8d85738d5e4bb3505
guthias <gmgustavom@gmail.com>
2021-12-06 16:38:27 -0300

    Realizando b7d1p2e1

---
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-01-let-const-e-arrow-function/exercicios/parte-2/exercicio-1.js
@@ -0,0 +1,15 @@
+function fatorial(n) {
+  let fatorial = 1;
+  for (let count = n; count > 1; count --) {
+    fatorial = fatorial * count;
+  }
+  return fatorial
+}
+
+console.log(fatorial(0)); // 1
+console.log(fatorial(1)); // 1
+console.log(fatorial(2)); // 2
+console.log(fatorial(3)); // 6
+console.log(fatorial(4)); // 24
+console.log(fatorial(5)); // 120
+console.log(fatorial(6)); // 720
