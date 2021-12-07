cf8c37797b665d20dfe78a08969ffe353fd5f75e
guthias <gmgustavom@gmail.com>
2021-12-07 18:49:57 -0300

    Criando arquivo para o b7d2 exericio bonus

---
+++ fundamentos/bloco-07-introducao-es6-e-testes-unitarios/dia-02-fluxo-de-excecao-e-objetos/exercicios/bonus.js
@@ -0,0 +1,20 @@
+const allLessons = {
+  lesson1: {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã'
+  },
+  lesson2: {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+    turno: 'noite'
+  },
+  lesson3: {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite'
+  }
+}
