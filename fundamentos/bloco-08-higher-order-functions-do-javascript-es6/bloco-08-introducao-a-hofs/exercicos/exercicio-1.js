0a83ab354936c1952b82afa1a09a517f54acae9a
guthias <gmgustavom@gmail.com>
2022-01-04 15:51:20 -0300

    Realizando b8d1e1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/bloco-08-introducao-a-hofs/exercicos/exercicio-1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/bloco-08-introducao-a-hofs/exercicos/exercicio-1.js
@@ -1,8 +1,17 @@
+const generateEmployer = name => {
+  return {
+    fullName: name,
+    email: name.toLowerCase().replace(' ', '_') + '@trybe.com'
+  }
+}
+
 const newEmployees = () => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: generateEmployer('Pedro Guerra'),
+    id2: generateEmployer('Luiza Drumond'),
+    id3: generateEmployer('Carla Paiva')
   }
   return employees;
 };
+
+// console.log(newEmployees());
