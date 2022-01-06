f311eefe64847e9b0316276d3f9cfecb29d93b17
guthias <gmgustavom@gmail.com>
2022-01-05 23:01:13 -0300

    Arquivo para fixar every b8d2

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/para-fixar/every.js
@@ -0,0 +1,13 @@
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+}
+
+console.log(verifyAges(people, 18));
