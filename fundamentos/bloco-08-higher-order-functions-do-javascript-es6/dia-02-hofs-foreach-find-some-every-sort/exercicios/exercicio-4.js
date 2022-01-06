976f6536f38ff158e3032b230aba6f0daf0dc010
guthias <gmgustavom@gmail.com>
2022-01-06 00:21:55 -0300

    Realizando b8d2e4

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/exercicios/exercicio-4.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/exercicios/exercicio-4.js
@@ -1,1 +1,5 @@
 const books = require('./template');
+
+const booksOrderedByReleaseYearDesc = () => books.sort((a, b) => b.releaseYear - a.releaseYear);
+
+console.log(booksOrderedByReleaseYearDesc())
