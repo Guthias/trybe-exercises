dd148117ce9bcd2e6c4cfb56cb91dfc3771950a8
guthias <gmgustavom@gmail.com>
2022-01-06 00:52:38 -0300

    Realizando b8d2e7

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/exercicios/exercicio-7.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/exercicios/exercicio-7.js
@@ -1,1 +1,11 @@
 const books = require('./template');
+
+function authorUnique() {
+  return books.every(book => {
+    return !books.some(bookSome => {
+      return bookSome.author.birthYear === book.author.birthYear && bookSome.author.name !== book.author.name
+    })
+  })
+}
+
+console.log(authorUnique())
