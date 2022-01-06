13c570d0cb466710a58fd433efa6db24ed754d17
guthias <gmgustavom@gmail.com>
2022-01-05 21:49:11 -0300

    Realizando para fixar find-1

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/para-fixar/find-1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-hofs-foreach-find-some-every-sort/para-fixar/find-1.js
@@ -1,7 +1,5 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const findDivisibleBy3And5 = () => {
-  // Adiciona seu código aqui
-}
+const findDivisibleBy3And5 = numbers => numbers.find(n => n % 3 === 0 && n % 5 === 0);
 
-console.log(findDivisibleBy3And5())
+console.log(findDivisibleBy3And5(numbers))
