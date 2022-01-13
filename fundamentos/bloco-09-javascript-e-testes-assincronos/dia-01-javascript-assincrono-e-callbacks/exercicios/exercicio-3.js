00cc586e585e76bd5c26a4c35ae5a36d52eae86a
unknown <gmgustavom@gmail.com>
2022-01-13 19:11:06 -0300

    Realizando b9d1e3

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio-3.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio-3.js
@@ -1,12 +1,12 @@
 const getPlanet = () => {
-    const mars = {
-      name: "Mars",
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: "kilometers",
-      },
-    };
-    console.log("Returned planet: ", mars);
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
   };
+  console.log("Returned planet: ", mars);
+};
   
-  getPlanet(); // imprime Marte depois de 4 segundos
+setTimeout(getPlanet, 4000);
