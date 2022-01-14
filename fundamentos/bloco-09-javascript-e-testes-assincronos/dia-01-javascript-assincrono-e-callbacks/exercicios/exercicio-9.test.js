2a17b12de64650ef3dc7732c02a07f203af226d7
unknown <gmgustavom@gmail.com>
2022-01-13 23:35:26 -0300

    Adicionando arquivo b9d1e9

---
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio-9.test.js
@@ -0,0 +1,11 @@
+const { getPokemonDetails } = require('./exercicio-8');
+
+describe('A função getPokemonDetails', () => {
+  it('retorna erro quando procuramos um pokemon que não existe no banco de dados', () => {
+    // Escreva aqui seu código
+  });
+
+  it('retorna um pokemon que existe no banco de dados', () => {
+    // Escreva aqui seu código
+  });
+});
