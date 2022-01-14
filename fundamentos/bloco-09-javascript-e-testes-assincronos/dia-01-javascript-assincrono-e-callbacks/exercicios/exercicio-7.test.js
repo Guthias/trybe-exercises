d1c0d68b20e385d8fee234a5cafbef8b6bdf32e4
unknown <gmgustavom@gmail.com>
2022-01-13 23:03:15 -0300

    Realizando exercicio b9d1e7

--- fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio-7.test.js
+++ fundamentos/bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercicios/exercicio-7.test.js
@@ -1,1 +1,14 @@
 const uppercase = require('./exercicio-7');
+
+describe('Implementando casos de teste para o Bloco 9 Dia 1 Exercicio 7', () => {
+  it('Verifica a função retorna a String passada como parametro como maiuscula', (done) => {
+    uppercase('guthias', (result) => {
+      try {
+        expect(result).toBe('GUTHIAS');
+        done();
+      } catch (error) {
+        done(error);
+      }
+    });
+  })
+})
