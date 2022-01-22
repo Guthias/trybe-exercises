056ddf582f0154a838c4fe8531417612425a7ea0
unknown <gmgustavom@gmail.com>
2022-01-22 01:07:02 -0300

    Realizando para fixar b10d1

--- front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/para-fixar/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Component from './Component';
 
 function App() {
-  return ();
+  return (
+    <Component/>
+  );
 }
 
 export default App;
