375d237eb834564a10a1e7121424e9c18d79d0bb
unknown <gmgustavom@gmail.com>
2022-01-22 02:11:16 -0300

    Realizando exercicio b10d1e2

--- front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios/exercicio-01-e-02/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/exercicios/exercicio-01-e-02/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <li>{value}</li>
+  );
+}
+
+function App() {
+  const taskList = ['Acordar', 'Ligar o computador', 'Abrir o vs code', 'Codar', 'Dormir'];
+  return (
+    <div>
+      <h1>Lista de tarefas</h1>
+      <ol>
+        { taskList.map(task => Task(task)) }
+      </ol>
     </div>
   );
 }
