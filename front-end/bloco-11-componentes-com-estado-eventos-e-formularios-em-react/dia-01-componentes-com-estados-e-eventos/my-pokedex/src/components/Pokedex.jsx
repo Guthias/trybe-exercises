bb8b9f55ff0eb6a0ba46de20f60986e80db2a060
unknown <gmgustavom@gmail.com>
2022-01-27 01:01:12 -0300

    Criando input e mostrando conteudo no handleChange

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-em-react/dia-01-componentes-com-estados-e-eventos/my-pokedex/src/components/Pokedex.jsx
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-em-react/dia-01-componentes-com-estados-e-eventos/my-pokedex/src/components/Pokedex.jsx
@@ -3,10 +3,17 @@ import pokemons from "../data";
 import Pokemon from "./Pokemon";
 
 class Pokedex extends React.Component {
+  handleChange({ target }) {
+    console.log(target.value);
+  }
+
   render () {
     return (
       <>
         <h1 className="pokedex-title">Pokedex</h1>
+
+        <input onChange={this.handleChange} type="text"/>
+
         <div className="pokedex">
           { pokemons.map((pokemon) => (
             <Pokemon key={pokemon.id} pokemon={pokemon}/> 
