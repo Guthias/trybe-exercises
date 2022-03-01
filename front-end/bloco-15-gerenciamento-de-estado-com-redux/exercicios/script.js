9672a6861a5ebfe64e00623d41320d6438987f42
unknown <gmgustavom@gmail.com>
2022-02-28 22:17:44 -0300

    creating store

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/exercicios/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/exercicios/script.js
@@ -1,4 +1,12 @@
-const ESTADO_INICIAL = {
+const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
 };
+
+function colorReducer(state = INITIAL_STATE, action) {
+  return state;
+}
+
+const store = Redux.createStore(colorReducer);
+
+console.log(store.getState());
