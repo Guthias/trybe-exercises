4114aca289df9ef3b9d400d27b08928339871dfb
unknown <gmgustavom@gmail.com>
2022-03-01 19:50:34 -0300

    sending actions when click on buttons

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/exercicios/script.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/exercicios/script.js
@@ -1,3 +1,6 @@
+const nextButton = document.getElementById('next');
+const previousButton = document.getElementById('previous');
+
 const INITIAL_STATE = {
   colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
   index: 0,
@@ -8,18 +11,25 @@ function colorReducer(state = INITIAL_STATE, action) {
     case 'NEXT_COLOR':
       return {
         ...state,
-        index: state.index < state.colors.length ? index + 1 : state.colors.length,
+        index: state.index < state.colors.length ? state.index + 1 : state.colors.length,
       }
     case 'PREVIOUS_COLOR':
       return {
         ...state,
-        index: state.index > 0 ? index - 1 : 0,
+        index: state.index > 0 ? state.index - 1 : 0,
       }
     default:
       return state
   }
 }
 
-const store = Redux.createStore(colorReducer);
+const store = Redux.createStore(colorReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-console.log(store.getState());
+previousButton.addEventListener('click', () => {
+  store.dispatch({ type: 'PREVIOUS_COLOR' });
+});
+
+
+nextButton.addEventListener('click', () => {
+  store.dispatch({ type: 'NEXT_COLOR' });
+});
